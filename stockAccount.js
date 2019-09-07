a6b2d1c80bd7511de686681600da5c5f8d8fe8dd
kran97 <kgupta.1697@gmail.com>
2019-09-07 18:37:14 +0530

    saturday2

--- stockAccount.js
+++ stockAccount.js
@@ -1,8 +1,62 @@
-const fs  = require('fs');
-const read = require('readline-sync');
-const utils = require('./utility/stockAccUtil');
+/******************************************************************************
+ * Execution    :   default node            terminal> node stockAccount.js
+ * 
+ * Purpose      :   To read input from JSON file and calculate the price of the
+ *                  grocery item and writing the result in another JSON file
+ * 
+ * @description
+ * 
+ * @file        :   stockAccount.js
+ * @overview    :   To read input from JSON file and calculate the price of the
+ *                  grocery item and writing the result in another JSON file
+ * @author      :   Karan Gupta
+ * @version     :   1.0
+ * @since       :   07-09-2019
+*******************************************************************************/
 
+const fs  = require('fs');                              //file-system
+const read = require('readline-sync');                  //user input library
+const utils = require('./utility/stockAccUtil');        //Utility file
+const obj1 = [];
+/**
+ * @description : reading stock market file and parsing it
+ */
 var data = fs.readFileSync('./json/stockInput.json','utf-8');
 var content = JSON.parse(data);
+console.log(content);
+
+/**
+ * @description : reading in user's portfolio and parsing it
+ */
+var userData = fs.readFileSync('./json/userStockFile.json','utf-8');
+var userContent = JSON.parse(userData);
+
+
 var obj = new utils.stockAccount();
-obj.buy(content);
+while(1)
+{
+    console.log("Press 1 for buying");
+    console.log("Press 2 for selling");
+    console.log("Press 3 for saving in files");
+    console.log("Press 4 for print final report");
+    console.log("Press any key for exit");
+    var choice = read.question("Enter your choice : ");
+    switch(choice)
+    {
+        case '1':
+            obj.buy(content, userContent, obj1);
+            break;
+        case '2':
+            obj.sell(content, userContent);
+            break;
+        case '3':
+            obj.write(content, userContent);
+            break;
+        case '4':
+            obj.print(content, userContent);
+            break;
+        default:
+            process.exit();
+            break;
+    }
+}
--- utility/stockAccUtil.js
+++ utility/stockAccUtil.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 const read = require('readline-sync');
+class customStokeAcc
+{
+    constructor()
+    {
+        this.name = '';
+        this.number = 0;
+        this.price = 0;
+        this.time = null;
+    }
+}
 class stockAccount
 {
     constructor()
@@ -8,10 +18,10 @@ class stockAccount
         this.number = 0;
         this.amount = 0;
     }
-    buy(content)
+    buy(content, userContent, obj1)
     {
+        var numStock = parseInt(read.question("Enter the number of companies : "));
         try{
-            var numStock = read.question("Enter the number of stocks : ");
             if (isNaN(numStock)) throw "invalid input"
         }
         catch (err) {
@@ -20,30 +30,126 @@ class stockAccount
         for(var i =0; i<numStock; i++)
         {
             try{
-            var name = read.question("Enter stock name : ");
-            if (!isNaN(name)) throw "invalid input"
-            var number = parseInt(read.question('enter the number of share: '));
-            if (isNaN(number)) throw "invalid input"
-            var price = parseInt(read.question('enter the price: '));
-            if (isNaN(price)) throw "invalid input"
-            
-            content.push({
-                "name" : name,
-                "price" : price,
-                "number" : number
-            })
+                var name = read.question("Enter stock name : ");
+                if (!isNaN(name)) throw "invalid input"
+                var number = parseInt(read.question('enter the number of share: '));
+                if (isNaN(number)) throw "invalid input"
+                var price = parseInt(read.question('enter the price: '));
+                if (isNaN(price)) throw "invalid input"
+                var flag=0;
+                for(var i in content)
+                {
+                    if(name == content[i].name && number<=content[i].number)
+                    { 
+                        for(var j in userContent)
+                        {
+                            if(name==userContent[j].name)
+                            {
+                                userContent[j].number+=number;
+                                break;
+                            }
+                            else
+                            {
+                                userContent.push({
+                                    "name" : name,
+                                    "price" : price,
+                                    "number" : number
+                                })
+                                break;
+                            }
+                        }
+                        flag=1;
+                        content[i].number-=number;
+                    }
+                }
+                if(flag==0)
+                {
+                    console.log("No Such Stock");
+                }
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+        var i=0;
+        obj1[i] = new customStokeAcc();
+        obj1[i].name = name;
+        obj1[i].number = number;
+        obj1[i].price = price;
+        //var dat = new Date();
+        obj1[i].time = Date(Date.now());
+        var outString = JSON.stringify(obj1, null, 2);
+        fs.writeFileSync('./json/stockTransaction.json', outString);
+        i++;
+        
+        console.log(content);
+        console.log("*********************");
+        console.log("USER's PORTFOLIO");
+        console.log(userContent);
+    }
+    sell(content, userContent)
+    {
+        var numDelStock;
+        var delName = read.question("Enter the stock you wanna sell : ");
+        for(var i in userContent)
+        {
+            try{
+                if(delName == userContent[i].name)
+                {
+                    numDelStock = parseInt(read.question("How much you want to sell? "));
+                    try{
+                        if(isNaN(numDelStock)) throw 'Please enter a number'
+                        if(numDelStock>userContent[i].number) throw 'You do not have enough stocks'
+                        userContent[i].number-=numDelStock;
+                        if(userContent[i].number == 0)
+                        {
+                            delete userContent[i];
+                        }
+                        for(var j in content)
+                        {
+                            if(delName == content[j].name)
+                            {
+                                content[j].number+=numDelStock;
+                            }
+                        }
+                    }
+                    catch (err) {
+                        console.log(err);
+                    }
+                }
             }
             catch (err) {
                 console.log(err);
             }
         }
         console.log(content);
+        console.log("*********************");
+        console.log("USER's PORTFOLIO");
+        console.log(userContent);
     }
-    sell(content)
+    write(content, userContent)
     {
-        var delName = read.question("Enter the ")
+        /**
+         * @description : Stock market file stringified and written
+         */
+        var outContent = JSON.stringify(content, null, 2);
+        fs.writeFileSync('./json/stockInput.json', outContent);
+
+        /**
+         * @description : User portfolio file stringified and written
+         */
+        var outUserContent = JSON.stringify(userContent, null, 2);
+        fs.writeFileSync('./json/userStockFile.json', outUserContent);
+    }
+    print(content, userContent)
+    {
+        console.log(content);
+        console.log("*********************");
+        console.log("USER's PORTFOLIO");
+        console.log(userContent);
     }
 }
 module.exports = {
+    customStokeAcc,
     stockAccount
 }
